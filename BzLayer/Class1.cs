9ca70f0dca3e7ada1ead16ed911d36d31fe0130f
sanchitamore23 <sanchitamore2305@gmail.com>
2023-10-20 11:36:26 +0000

    first commit

---
+++ BzLayer/Class1.cs
@@ -0,0 +1,11 @@
+﻿using Entity;
+using DataLayer;
+namespace BzLayer;
+public class MoviesBz
+{
+  public List<Movie> GetMovies(){
+  DataAccess dataAccess=new DataAccess();
+  return dataAccess.GetMovies();
+ 
+   }
+}
